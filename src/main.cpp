13538028ed0eccc06d7b524f4e6368d13d41e685
komanduriabh <komanduriab@gmail.com>
2020-07-14 16:25:29 -0500

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
+		pros::lcd::set_text(2, "I was not pressed!");
 	} else {
 		pros::lcd::clear_line(2);
 	}
